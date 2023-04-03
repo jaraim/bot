11e493f2debf517f931f87f64f3b08bd62d5c674
jaraim <117310975+jaraim@users.noreply.github.com>
2023-04-03 19:51:33 +0800

    Update ffmpeg_stream.sh

--- ffmpeg_stream.sh
+++ ffmpeg_stream.sh
@@ -80,32 +80,38 @@ stream_start() {
     fi
 
     # 定义视频存放目录
-    read -p "输入你的视频存放目录 (格式仅支持mp4,并且要绝对路径,例如/opt/video):" folder
+read -p "输入你的视频存放目录 (格式仅支持mp4,并且要绝对路径,例如/opt/video):" folder
 
-    # 判断是否需要添加水印
-    read -p "是否需要为视频添加水印?水印位置默认在右上方,需要较好CPU支持(yes/no):" watermark
-    if [[ $watermark = "yes" ]]; then
-        read -p "输入你的水印图片存放绝对路径,例如/opt/image/watermark.jpg (格式支持jpg/png/bmp):" image
-        echo -e "${yellow} 添加水印完成,程序将开始推流。${font}"
-        # 循环
-        while true; do
-            cd $folder
-            for video in $(ls *.mp4); do
-                ffmpeg -re -i "$video" -i "$image" -filter_complex overlay=W-w-5:5 -c:v libx264 -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}&
-            done
-        done
-    fi
-    if [[ $watermark = "no" ]]; then
-        echo -e "${yellow} 你选择不添加水印,程序将开始推流。${font}"
-        # 循环
-        while true; do			
-            cd $folder		
-            for video in $(ls *.mp4); do
-                ffmpeg -re -i "$video" -c:v copy -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}&
-            done
-        done
-    fi			
-}	
+# 判断是否需要添加水印
+read -p "是否需要为视频添加水印?水印位置默认在右上方,需要较好CPU支持(yes/no):" watermark
+if [ $watermark = "yes" ];then
+	read -p "输入你的水印图片存放绝对路径,例如/opt/image/watermark.jpg (格式支持jpg/png/bmp):" image
+	echo -e "${yellow} 添加水印完成,程序将开始推流. ${font}"
+	# 循环
+	while true
+	do
+		cd $folder
+		for video in $(ls *.mp4)
+		do
+		ffmpeg -re -i "$video" -i "$image" -filter_complex overlay=W-w-5:5 -c:v libx264 -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
+		done
+	done
+fi
+if [ $watermark = "no" ]
+then
+    echo -e "${yellow} 你选择不添加水印,程序将开始推流. ${font}"
+    # 循环
+	while true
+	do
+		cd $folder
+		for video in $(ls *.mp4)
+		do
+		ffmpeg -re -i "$video" -c:v copy -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
+		done
+	done
+fi
+	}
+
 
 # 停止推流
 stream_stop() {
