6cb8c7fd3646aa3d3bf1d7d73678bdcb2a0c1eaa
jaraim <117310975+jaraim@users.noreply.github.com>
2023-04-03 15:07:14 +0800

    Update ffmpeg_stream.sh

--- ffmpeg_stream.sh
+++ ffmpeg_stream.sh
@@ -30,7 +30,6 @@ fi
 #创建新目录
 mkdir /home/lighthouse/ffmpg
 cd /home/lighthouse/ffmpg
-
 ffmpeg_install(){
 # 安装FFMPEG
 read -p "你的机器内是否已经安装过FFmpeg4.x?安装FFmpeg才能正常推流,是否现在安装FFmpeg?(yes/no):" Choose
@@ -126,4 +125,5 @@ start_menu(){
 
 # 运行开始菜单
 start_menu
+
 EOF
