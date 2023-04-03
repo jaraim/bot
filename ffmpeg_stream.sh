8e01887d663c3d780b8cbd01d085d2e5cf242859
jaraim <117310975+jaraim@users.noreply.github.com>
2023-04-03 14:09:25 +0800

    Update ffmpeg_stream.sh

--- ffmpeg_stream.sh
+++ ffmpeg_stream.sh
@@ -33,35 +33,35 @@ mkdir /home/lighthouse/ffmpg
 cd /home/lighthouse/ffmpg
 # 安装screen
 if [[ $(command -v screen) ]]; then
-	echo "screen已安装"
+  echo "screen已安装"
 else
-	echo "screen未安装,开始安装screen"
-	if [[ $(command -v apt-get) ]]; then
-	sudo apt-get install -y screen
-	elif [[ $(command -v yum) ]]; then
-	sudo yum install -y screen
-	elif [[ $(command -v dnf) ]]; then
-	sudo dnf install -y screen
-	else
-		echo "不支持的操作系统" && exit 1
-	fi
+  echo "screen未安装,开始安装screen"
+  if [[ $(command -v apt-get) ]]; then
+  sudo apt-get install -y screen
+  elif [[ $(command -v yum) ]]; then
+  sudo yum install -y screen
+  elif [[ $(command -v dnf) ]]; then
+  sudo dnf install -y screen
+  else
+    echo "不支持的操作系统" && exit 1
+  fi
 fi
 ffmpeg_install(){
 # 安装FFMPEG
 read -p "你的机器内是否已经安装过FFmpeg4.x?安装FFmpeg才能正常推流,是否现在安装FFmpeg?(yes/no):" Choose
 if [ $Choose = "yes" ];then
-	yum -y install wget
-	wget --no-check-certificate https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.0.3-64bit-static.tar.xz
-	tar -xJf ffmpeg-4.0.3-64bit-static.tar.xz
-	cd ffmpeg-4.0.3-64bit-static
-	mv ffmpeg /usr/bin && mv ffprobe /usr/bin && mv qt-faststart /usr/bin && mv ffmpeg-10bit /usr/bin
+  yum -y install wget
+  wget --no-check-certificate https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.0.3-64bit-static.tar.xz
+  tar -xJf ffmpeg-4.0.3-64bit-static.tar.xz
+  cd ffmpeg-4.0.3-64bit-static
+  mv ffmpeg /usr/bin && mv ffprobe /usr/bin && mv qt-faststart /usr/bin && mv ffmpeg-10bit /usr/bin
 fi
 if [ $Choose = "no" ]
 then
     echo -e "${yellow} 你选择不安装FFmpeg,请确定你的机器内已经自行安装过FFmpeg,否则程序无法正常工作! ${font}"
     sleep 2
 fi
-	}
+  }
 
 stream_start(){
 # 定义推流地址和推流码
@@ -69,11 +69,11 @@ read -p "输入你的推流地址和推流码(rtmp协议):" rtmp
 
 # 判断用户输入的地址是否合法
 if [[ $rtmp =~ "rtmp://" ]];then
-	echo -e "${green} 推流地址输入正确,程序将进行下一步操作. ${font}"
-  	sleep 2
-	else  
-  	echo -e "${red} 你输入的地址不合法,请重新运行程序并输入! ${font}"
-  	exit 1
+  echo -e "${green} 推流地址输入正确,程序将进行下一步操作. ${font}"
+    sleep 2
+  else  
+    echo -e "${red} 你输入的地址不合法,请重新运行程序并输入! ${font}"
+    exit 1
 fi 
 
 # 定义视频存放目录
@@ -82,38 +82,38 @@ read -p "输入你的视频存放目录 (格式仅支持mp4,并且要绝对路�
 # 判断是否需要添加水印
 read -p "是否需要为视频添加水印?水印位置默认在右上方,需要较好CPU支持(yes/no):" watermark
 if [ $watermark = "yes" ];then
-	read -p "输入你的水印图片存放绝对路径,例如/opt/image/watermark.jpg (格式支持jpg/png/bmp):" image
-	echo -e "${yellow} 添加水印完成,程序将开始推流. ${font}"
-	# 循环
-	while true
-	do
-		cd $folder
-		for video in $(ls *.mp4)
-		do
-		ffmpeg -re -i "$video" -i "$image" -filter_complex overlay=W-w-5:5 -c:v libx264 -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
-		done
-	done
+  read -p "输入你的水印图片存放绝对路径,例如/opt/image/watermark.jpg (格式支持jpg/png/bmp):" image
+  echo -e "${yellow} 添加水印完成,程序将开始推流. ${font}"
+  # 循环
+  while true
+  do
+    cd $folder
+    for video in $(ls *.mp4)
+    do
+    ffmpeg -re -i "$video" -i "$image" -filter_complex overlay=W-w-5:5 -c:v libx264 -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
+    done
+  done
 fi
 if [ $watermark = "no" ]
 then
     echo -e "${yellow} 你选择不添加水印,程序将开始推流. ${font}"
     # 循环
-	while true
-	do
-		cd $folder
-		for video in $(ls *.mp4)
-		do
-		ffmpeg -re -i "$video" -c:v copy -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
-		done
-	done
+  while true
+  do
+    cd $folder
+    for video in $(ls *.mp4)
+    do
+    ffmpeg -re -i "$video" -c:v copy -c:a aac -b:a 192k -strict -2 -f flv ${rtmp}
+    done
+  done
 fi
-	}
+  }
 
 # 停止推流
 stream_stop(){
-	screen -S stream -X quit
-	killall ffmpeg
-	}
+  screen -S stream -X quit
+  killall ffmpeg
+  }
 
 # 开始菜单设置
 echo -e "${yellow} CentOS7 X86_64 FFmpeg无人值守循环推流 For LALA.IM ${font}"
@@ -137,7 +137,7 @@ start_menu(){
         echo -e "${red} 请输入正确的数字 (1-3) ${font}"
         ;;
     esac
-	}
+  }
 
 # 运行开始菜单
 start_menu
