9f759cd48ab706234e643345af5360de9ae3406a
jaraim <117310975+jaraim@users.noreply.github.com>
2023-04-04 23:54:21 +0800

    Update replit.sh

--- replit.sh
+++ replit.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-#检测wget有没有安装，没有就安装
-if [ ! -x "$(command -v wget)" ]; then
-  echo 'Error: wget is not installed.' >&2
-  echo 'Installing wget...'
-  sudo apt-get install wget
-fi  
 #下载replit.py
-wget https://raw.githubusercontent.com/jaraim/bot/main/replit.py
+curl -s -L https://raw.githubusercontent.com/jaraim/bot/main/replit.py
 #运行replit.py
 python3 replit.py
