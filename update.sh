c7a1a9e33608538334ac5ed96874b7282c0cb503
jaraim <117310975+jaraim@users.noreply.github.com>
2023-04-09 16:20:13 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-#!/bin/bash
-
 # 获取当前操作系统的名称
 OS=$(uname -s)
-
 # 检查操作系统是否为Ubuntu
 if [ "$OS" = "Linux" ] && [ -x "$(command -v apt-get)" ]; then
   echo "当前系统为Ubuntu"
@@ -32,18 +28,21 @@ else
     echo "当前系统不是CentOS"
   fi
 fi
-# 检查Python3和pip3是否已安装，已安装则跳过，未安装则安装
-if command -v python3 &> /dev/null
-then
-    echo "Python3已安装"
+# 检查Python 3是否已安装
+if [ -x "$(command -v python3)" ]; then
+  echo "Python 3已经安装"
 else
-    echo "Python3未安装，正在安装"
-    sudo $PKG_MANAGER install python3
+  echo "Python 3未安装。正在安装..."
+  sudo apt-get update
+  sudo apt-get install python3
 fi
-if command -v pip3 &> /dev/null
-then
-    echo "pip3已安装"
+
+# 检查pip3是否已安装
+if [ -x "$(command -v pip3)" ]; then
+  echo "pip3已经安装"
 else
-    echo "pip3未安装，正在安装"
-    sudo $PKG_MANAGER install python3-pip
+  echo "pip3未安装。正在安装..."
+  sudo apt-get update
+  sudo apt-get install python3-pip
 fi
+echo "Python 3和pip3安装完成"
