07fe9fce7b8d96ba7b0a257e1f3f62fc67cea2d0
jaraim <117310975+jaraim@users.noreply.github.com>
2023-04-09 16:28:03 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 # 获取当前操作系统的名称
 OS=$(uname -s)
+
 # 检查操作系统是否为Ubuntu
 if [ "$OS" = "Linux" ] && [ -x "$(command -v apt-get)" ]; then
   echo "当前系统为Ubuntu"
   echo "apt-get已安装"
 else
-  if [ "$OS" = "Linux" ]; then
     echo "当前系统为Ubuntu"
+    echo "当前系统不是CentOS"
     echo "apt-get未安装，开始安装..."
     sudo apt-get update
-  else
-    echo "当前系统不是CentOS"
-  fi
 fi
 # 检查操作系统是否为CentOS
 if [ "$OS" = "Linux" ] && [ -x "$(command -v yum)" ]; then
   echo "当前系统为CentOS"
   echo "yum已安装"
 else
-  if [ "$OS" = "Linux" ]; then
     echo "当前系统为CentOS"
+    echo "当前系统不是Ubuntu"
     echo "yum未安装，开始安装..."
     sudo yum update
-  else
-    echo "当前系统不是Ubuntu"
-  fi
 fi
 # 检查Python 3是否已安装
 if [ -x "$(command -v python3)" ]; then
