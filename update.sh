1f0f8334a0b76a7acee883d2b3b75764fc8f0bd9
jaraim <117310975+jaraim@users.noreply.github.com>
2023-06-16 01:37:47 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -38,20 +38,32 @@ else
     fi
 fi
 
-  # 检查pip3是否已安装
+ # 检查pip3是否已安装
 if [ -x "$(command -v pip3)" ]; then
     echo "pip3已经安装"
+    
+# 检查pip版本是否为最新，如果不是则进行升级
+    if [ "$(pip3 --version | awk '{print $2}' | cut -d'.' -f1)" -lt 21 ]; then
+        echo "pip版本不是最新版。正在升级..."
+        pip3 install --upgrade pip
+    fi
 else
     echo "pip3未安装。正在安装..."
-    if [ -x "$(command -v apt-get)" ]; then
-      sudo apt-get update
-      sudo apt-get install -y python3-pip
-      python3 -m pip install --upgrade pip
-    else
-      sudo yum update
-      sudo yum install -y python3-pip
-      python3 -m pip install --upgrade pip
-    fi
+    
+   
+if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get update
+        sudo apt-get install -y python3-pip
+    
+        sudo apt-get update
+        sudo apt-get install -y
+else
+        sudo yum update
+        sudo yum install -y python3-pip
+    
+        sudo yum update
+        sudo yum install -y python3-pip
+fi
 fi
 # 检查Python 3和pip3是否安装成功
 echo "Python 3和pip3安装完成"
