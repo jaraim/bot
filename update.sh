598848b89c187bf8aec6b97fe9a20f480043746b
jaraim <117310975+jaraim@users.noreply.github.com>
2023-06-26 01:57:03 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ if [ "$OS" = "Linux" ]; then
     if [ -x "$(command -v apt-get)" ]; then
         echo "当前系统为Ubuntu"
         echo "apt-get已安装"
+        sudo apt-get update
+        sudo apt-get upgrade -y
     else
         echo "当前系统为Ubuntu"
         echo "apt-get未安装，开始安装..."
@@ -16,10 +18,12 @@ else
     if [ -x "$(command -v yum)" ]; then
         echo "当前系统为CentOS"
         echo "yum已安装"
+        sudo yum update -y
+        sudo yum upgrade -y
     else
         echo "当前系统为CentOS"
         echo "yum未安装，开始安装..."
-        sudo yum update
+        sudo yum update -y
         sudo yum install -y yum
     fi  
 fi
